cf656cc6f68e1ff3270742b5e2888311d6949c8a
Vladimir Mironov <vladimir.i.mironov@gmail.com>
2014-03-21 14:39:18 +0400

    send mail workaround

--- send_ip.sh
+++ send_ip.sh
@@ -3,9 +3,10 @@
 . /tools/config.cfg
 . /tools/functions.sh
 
-WHO=vladimir_mironov@epam.com,vladimir.i.mironov@gmail.com
+WHO=vladimir_mironov@epam.com
+#,vladimir.i.mironov@gmail.com
 
-report "[RSREU]: external ip-address" "FYI: <b>$(/sbin/ip addr show ppp0 | grep inet|awk '{print $2}')</b>"
+report "[RSREU]: external ip-address" ": <b>$(/sbin/ip addr show ppp0 | grep inet|awk '{print $2}') :</b>"
 # if [ -a $DEVICE ]; then echo ooo;fi
 # if [ -a /dev/ttyUSB0 ]; then echo aaa;fi
 
